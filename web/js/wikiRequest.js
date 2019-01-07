a7f1f3da3180535a316aed3141d7cef873d7c9d6
陳泓文 <r07922037@ntu.edu.tw>
2019-01-07 17:24:49 +0800

    fix safari regex error

--- web/js/wikiRequest.js
+++ web/js/wikiRequest.js
@@ -75,7 +75,7 @@ function get_URL(urls, progress_update_callback, result_callback) {
 				data.data.filename = page.title;
 				data.data.wiki_metadata.url = event.data.url;
 				data.data.wiki_metadata.page_id = page.pageid;
-				data.data.doc_content = page.extract.replace(/(?<!\\)\n/g, "<br>"); // Change \n to <br> so that browsers can understand. Avoid '\\n'.
+				data.data.doc_content = page.extract.replace(/(?!\\)\n/g, "<br>"); // Change \n to <br> so that browsers can understand. Avoid '\\n'.
 				data.data.compilation = event.data.url;
 				insert_result(data);
 			}
